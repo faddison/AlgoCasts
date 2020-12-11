8657ab4aaf70e057f4df83f9f519a4ef2038b1fe
Fraser Addison <fraseraddison@gmail.com>
2020-12-11 14:14:26 -0800

    added tree and node files and skeleton classes

---
+++ exercises/tree/test_tree.py
@@ -0,0 +1,5 @@
+from tree import Node, Tree
+
+def test_tree_node():
+    n = Node(1)
+    assert n.data == 1
---
+++ exercises/tree/tree.py
@@ -0,0 +1,16 @@
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+class Tree(object):
+
+    def __init__():
+        pass
