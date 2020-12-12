098eba919ccc5d7e918904e38b9bbf131e3cf145
Fraser Addison <fraseraddison@gmail.com>
2020-12-11 16:17:58 -0800

    finished node, tree, and BFS

--- exercises/tree/test_tree.py
+++ exercises/tree/test_tree.py
@@ -1,5 +1,127 @@
 from tree import Node, Tree
 
-def test_tree_node():
+def test_tree_node_data():
     n = Node(1)
     assert n.data == 1
+
+def test_tree_node_add_child():
+    n = Node(1)
+    n.add(2)
+    assert n.children[0].data == 2
+
+def test_tree_node_add_childAndSibling():
+    n = Node(1)
+    n.add(2)
+    n.add(3)
+    assert n.children[0].data == 2
+    assert n.children[1].data == 3
+    
+def test_tree_node_add_grandchild():
+    n = Node(1)
+    n.add(2)
+    n.children[0].add(3)
+    assert n.children[0].children[0].data == 3
+
+def test_tree_node_remove_data_match():
+    n = Node(1)
+    n.add(2)
+    n.remove(2)
+    assert n.children[0] == None
+
+def test_tree_node_remove_data_mismatch():
+    n = Node(1)
+    n.add(2)
+    n.remove(3)
+    assert n.children[0] != None
+    
+def test_tree_node_remove_data_parentmatch():
+    n = Node(1)
+    n.remove(1)
+    try:
+        n.children[0]
+    except Exception as e:
+        assert str(e) == 'list index out of range'
+
+def test_tree_node_remove_data_multi_same_match():
+    n = Node(1)
+    n.add(2)
+    n.add(3)
+    n.remove(2)
+    n.remove(2)
+    assert n.children[0] == None
+    assert n.children[1].data == 3
+
+def test_tree_node_remove_data_multi_diff_match():
+    n = Node(1)
+    n.add(2)
+    n.add(3)
+    n.remove(2)
+    n.remove(3)
+    assert n.children[0] == None
+    assert n.children[1] == None
+
+def test_tree_traverseBF_root():
+    n = Node(0)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
+
+def test_tree_traverseBF_1child():
+    n = Node(0)
+    n.add(20)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
+
+def test_tree_traverseBF_1sibling():
+    n = Node(0)
+    n.add(20)
+    n.add(21)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
+
+def test_tree_traverseBF_1direct_grandchild():
+    n = Node(0)
+    n.add(20)
+    n.add(21)
+    n.children[0].add(30)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
+
+def test_tree_traverseBF_1direct_grandchild1sibling():
+    n = Node(0)
+    n.add(20)
+    n.add(21)
+    n.children[0].add(30)
+    n.children[0].add(31)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
+
+def test_tree_traverseBF_1indirect_grandchild():
+    n = Node(0)
+    n.add(20)
+    n.add(21)
+    n.children[1].add(30)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
+
+def test_tree_traverseBF_1both_grandchild1sibling():
+    n = Node(0)
+    n.add(20)
+    n.add(21)
+    n.children[0].add(30)
+    n.children[1].add(31)
+    t = Tree()
+    t.root = n
+    f = lambda n : print("data={0}".format(n.data))
+    t.traverseBF(f)
--- exercises/tree/tree.py
+++ exercises/tree/tree.py
@@ -1,16 +1,45 @@
+#   Directions
+#  1) Create a node class.  The constructor
+#  should accept an argument that gets assigned
+#  to the data property and initialize an
+#  empty array for storing children. The node
+#  class should have methods 'add' and 'remove'.
+#  2) Create a tree class. The tree constructor
+#  should initialize a 'root' property to null.
+#  3) Implement 'traverseBF' and 'traverseDF'
+#  on the tree class.  Each method should accept a
+#  function that gets called with each element in the tree
+
 class Node(object):
 
     def __init__(self, data):
         self.data = data
         self.children = []
 
-    def add(self):
-        pass
+    def add(self, data):
+        self.children.append(Node(data))
+        # print("add:len={0}".format(len(self.children)))
 
-    def remove(self):
-        pass
+    def remove(self, data):
+        # print("remove:len={0}".format(len(self.children)))
+        for i in range(0,len(self.children)):
+            if self.children[i] is not None and self.children[i].data == data:
+                self.children[i] = None
 
 class Tree(object):
 
-    def __init__():
+    def __init__(self):
+        self.root = None
+
+    def traverseBF(self, func):
+        nodes = []
+        nodes.append(self.root)
+        while len(nodes) > 0:
+            # print("len={0}".format(len(nodes)))
+            n = nodes[0]
+            nodes.pop(0)
+            func(n)
+            nodes.extend(n.children)
+
+    def traverseDF(self, func):
         pass
